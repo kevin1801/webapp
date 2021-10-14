4f3fe3e55eedb9df8173000eeb0535ed8b10b496
kevin1801 <jaffson@hotmail.com>
2021-10-14 11:14:18 +0800

    1410

--- react-ui/src/components/Template.js
+++ react-ui/src/components/Template.js
@@ -24,7 +24,7 @@ class Template extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm Yu Liu</h1>
+            <h1 className="responsive-headline">I'm </h1>
             <h3>I'm a Manila based <span>graphic designer</span>, <span>illustrator</span> and <span>webdesigner</span> creating awesome and
             effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling </a>
              and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
